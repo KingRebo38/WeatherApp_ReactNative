34b6efb1b96f026842075bad80ada36e471501fa
DrNossi <jojokraftus@gmail.com>
2021-06-25 16:04:50 +0200

    Menu und Wetter-Items der UI hinzugefügt

---
+++ ui_elements/Menu.js
@@ -0,0 +1,106 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {Component} from 'react';
+import {View, Text, Button, TextInput, StyleSheet, Image} from 'react-native';
+
+export default class Menu extends Component {
+
+    onPress = () => {
+        //placeholder
+    };
+
+    render() {
+        return (
+                <View style={styles.menu}>
+                    <View style={styles.burger}></View>
+                    <TextInput style={styles.searchbar}/>
+                    <Button title={'Suchen'} onPress={this.onPress}>
+                    </Button>
+                </View>
+        );
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor : '#fff'
+    },
+    menu:{
+        flexDirection: 'row',
+        height: 50,
+        alignItems: 'center',
+        backgroundColor: '#e6f2ff'
+    },
+    burger:{
+        height: 50,
+        width: 50,
+        marginRight : 12,
+        backgroundColor: '#99ccff'
+
+    },
+    searchbar:{
+        height: 40,
+        width: '65%',
+        marginRight: 5,
+        backgroundColor: '#fff'
+    },
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
