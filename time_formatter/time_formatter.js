8b07077315322f42921027d6648a844ccc44b4ba
Umut Armut <umutarmut1858@gmail.com>
2021-07-02 10:21:20 +0200

    Converted some of the values in the data model to represent actual readable metrics. Fixed an import issue in PocketView.js.

--- time_formatter/time_formatter.js
+++ time_formatter/time_formatter.js
@@ -1,10 +1,19 @@
 export class Time_formatter {
-    static getCurrentTime(){
-        var today = new Date()
-        var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-        var dateTime = date+' '+time;
+  static getCurrentTime() {
+    var today = new Date();
+    var date =
+      today.getFullYear() +
+      '-' +
+      (today.getMonth() + 1) +
+      '-' +
+      today.getDate();
+    var time =
+      today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+    var dateTime = date + ' ' + time;
 
-        return dateTime
-    }
+    return dateTime;
+  }
+  static getTimeOnly(date: Date) {
+    return date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+  }
 }
--- ui_elements/PocketView.js
+++ ui_elements/PocketView.js
@@ -1,27 +1,26 @@
 import {View, Text, StyleSheet} from 'react-native';
-import Icon from 'react-native-vector-icons/Feather'
+import Icon from 'react-native-vector-icons/Feather';
+import React, {Component} from 'react';
 const PocketView = props => {
-
-        return (
-            <View style={styles.container}>
-                <Text style={styles.citytext}>{props.city}</Text>
-                <Icon name="sun" size={100} color="#F0DA0B"/>
-                <Text style={styles.citytext}>{props.temp}</Text>
-            </View>
-        );
-
-}
+  return (
+    <View style={styles.container}>
+      <Text style={styles.citytext}>{props.city}</Text>
+      <Icon name="sun" size={100} color="#F0DA0B" />
+      <Text style={styles.citytext}>{props.temp}</Text>
+    </View>
+  );
+};
 const styles = StyleSheet.create({
-    container: {
-        alignItems: 'center',
-    },
+  container: {
+    alignItems: 'center',
+  },
 
-    weatherImage:{
-        width: 175,
-        height: 175,
-    },
-    citytext:{
-        fontSize: 30,
-    }
+  weatherImage: {
+    width: 175,
+    height: 175,
+  },
+  citytext: {
+    fontSize: 30,
+  },
 });
 export default PocketView;
